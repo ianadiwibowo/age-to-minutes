f149e1dc8aa4175a118327645878376af495710d
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-08-24 02:28:37 +0700

    Calculate and set age in minutes text

--- app/src/main/java/com/gitlab/ianadiwibowo/agetominutes/MainActivity.kt
+++ app/src/main/java/com/gitlab/ianadiwibowo/agetominutes/MainActivity.kt
@@ -3,8 +3,6 @@ package com.gitlab.ianadiwibowo.agetominutes
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -14,29 +12,64 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnSelectDate.setOnClickListener { view ->
-            clickDataPicker(view)
+        btnSelectDate.setOnClickListener { _ ->
+            clickSelectDate()
         }
     }
 
-    private fun clickDataPicker(view: View) {
+    private fun clickSelectDate() {
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
         val day = calendar.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(
+        val datePickerDialog = DatePickerDialog(
             this,
-            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
-                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
-                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
-                val date = sdf.parse(selectedDate)
-
-                tvSelectedDate.text = date.toString()
+            DatePickerDialog.OnDateSetListener { _, selectedYear, selectedMonth, selectedDay ->
+                setDatePicker(selectedDay, selectedMonth, selectedYear)
             },
             year,
             month,
             day
-        ).show()
+        )
+        datePickerDialog.datePicker.maxDate = Date().time
+        datePickerDialog.show()
+    }
+
+    private val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+    private fun setDatePicker(selectedDay: Int, selectedMonth: Int, selectedYear: Int) {
+        val (birthDate, birthDateInMinutes) = getDate(selectedDay, selectedMonth, selectedYear)
+
+        updateSelectedDateText(birthDate)
+        updateAgeInMinutesText(currentDateInMinutes() - birthDateInMinutes)
+    }
+
+    private fun getDate(selectedDay: Int, selectedMonth: Int, selectedYear: Int): Pair<Date, Long> {
+        val date = getSelectedDate(selectedDay, selectedMonth, selectedYear)
+        val dateInMinutes = convertToMinute(date)
+        return Pair(date, dateInMinutes)
+    }
+
+    private fun getSelectedDate(selectedDay: Int, selectedMonth: Int, selectedYear: Int): Date {
+        val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+        return dateFormat.parse(selectedDate) as Date
+    }
+
+    private fun convertToMinute(date: Date?): Long {
+        return date!!.time / 60_000
+    }
+
+    private fun currentDateInMinutes(): Long {
+        val currentDate = dateFormat.parse(dateFormat.format(System.currentTimeMillis()))
+        return convertToMinute(currentDate)
+    }
+
+    private fun updateSelectedDateText(birthDate: Date) {
+        tvSelectedDate.text = birthDate.toString()
+    }
+
+    private fun updateAgeInMinutesText(ageInMinutes: Long) {
+        tvAgeInMinutes.text = ageInMinutes.toString()
     }
 }
